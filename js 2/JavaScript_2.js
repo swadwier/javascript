48d2ce1e958bfc87ec1ea6158ef71e8c9dfb92c9
ульяна <swadwier@gmail.com>
2024-04-19 10:12:18 +0300

    версия перед изменениями

--- js 2/JavaScript_2.js
+++ js 2/JavaScript_2.js
@@ -31,27 +31,200 @@
 // // *) вариация: движутся несколько поездов. Alert выводить общий по всем поездам.
 
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 let stops = ["Санкт-Петербург", "Редкино", "Лихоборы", "Москва", "Арзамас-1", "Саранск-1"];
 let timeOuterInterval = [0, 5780, 680+2140,700+300,240+6520,520+3400];
+let realInterval = [0, getRandomInt(5680,5880),getRandomInt(2720,2920),getRandomInt(900, 1100),getRandomInt(6660, 6860), getRandomInt(3820,4020)]
 let date = new Date(2024, 3, 15, 17, 14, 35);
-// let way_label = {}
+let way_label = ''
 
-function way(stops, timeOuterInterval, i = 0) {
+function wayMode(){
+    if 
+
+}
+
+function way(stops, realInterval, i = 0) {
     if (i < stops.length) {
         setTimeout(() => {
-            let minutes = date.getMinutes()+(timeOuterInterval[i]/20);
+            let minutes = date.getMinutes()+(realInterval[i]/20);
             date.setMinutes(minutes);
-            // way_label[stops[i]] = date.toLocaleTimeString('it-IT');
-            console.log(`Поезд прибыл в ${stops[i]}`, `(${date.toLocaleTimeString('it-IT')})`, 'скоростной режим - по расписанию');
-            way(stops, timeOuterInterval, i + 1);
-        }, timeOuterInterval[i]);
+            way_label += `${stops[i]} - ${date.toLocaleTimeString('it-IT')}`;//строка
+            console.log(`Поезд прибыл в ${stops[i]}`, `(${date.toLocaleTimeString('it-IT')})`, `скоростной режим - по расписанию`);
+            way(stops, realInterval, i + 1);
+        }, realInterval[i]);
     }
 }
 
-way(stops, timeOuterInterval);
-alert(`Результат - поездка по расписанию`)
+way(stops, realInterval);
+alert(`Результат - поездка по расписанию, ${way_label}`)
     
+
+// 2) «Черепаший марафон»
+// Две черепахи бегут марафон – 40 км. 
+// За раз они могут пробежать только 4 часа со скоростью 2км/ч. 
+// После этого необходим отдых от 3-5 часов, причем если черепаха спит больше 4 часов, 
+// то каждые полные 15 минут добавляют ей возможность дополнительно пробежать 250м
+// к следующему отрезку дистанции. 
+// Произвести симуляцию забега с логированием всех событий в консоль.
+// Результаты забега вывести на страницу через alert.
+// Временной масштаб симуляции: 320мс:1ч.
+
+
+//A, B 
+//way = 40 km
+//at a go = 4h * 2km\h = 8 km
+//if sleep > 4 :
+//(sleep * 60 -(60/4))/15 = n
+//if n = 4 (5h), 250* 4 = +1000m = +1km
+
+//320мс:1ч
+//4h = 1280ms
+
+//Пусть время сна - рандомное число от 3 до 5
+
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// function tortWay() {
+//     const maxWay = 40;
+//     const speed = 2;
+//     const oneStepTime = 4;
+//     const oneStep = oneStepTime * speed;
+
+//     let aTort = {
+//         way: 0,
+//         sleep: 0,
+//         extra: 0,
+//         time: 0
+//     };
+
+//     let bTort = {
+//         way: 0,
+//         sleep: 0,
+//         extra: 0,
+//         time: 0
+//     };
+
+//     function run() {
+//         if (aTort.way < maxWay && bTort.way < maxWay) {
+
+//             aTort.way += oneStep + aTort.extra;
+//             aTort.time = (aTort.extra / speed) + oneStepTime;
+//             console.log(`Первая черепаха пробежала ${aTort.way} метров`);
+
+//             bTort.way += oneStep + bTort.extra;
+//             bTort.time = (bTort.extra / speed) + oneStepTime;
+//             console.log(`Вторая черепаха пробежала ${bTort.way} метров`);
+
+//             sleep();
+//         } else {д
+//             if (aTort.way >= maxWay) console.log("Первая черепаха достигла финиша!");
+//             if (bTort.way >= maxWay) console.log("Вторая черепаха достигла финиша!");
+//             return;
+//         }
+//     }
+
+//     function sleep() {
+//         aTort.sleep = getRandomInt(3, 5);
+//         bTort.sleep = getRandomInt(3, 5);
+
+//         if (aTort.sleep > 4) {
+//             aTort.extra = ((Math.floor((aTort.sleep * 60 - 60 / 4) / 15)) * 250) / 1000;
+//         } else {
+//             aTort.extra = 0;
+//         }
+
+//         if (bTort.sleep > 4) {
+//             bTort.extra = ((Math.floor((bTort.sleep * 60 - 60 / 4) / 15)) * 250) / 1000;
+//         } else {
+//             bTort.extra = 0;
+//         }
+
+//         setTimeout(run, Math.max(aTort.time * 320, bTort.time * 320));
+//     }
+
+//     sleep();
+// }
+
+// tortWay();
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+function tortWay(){
+    const maxWay = 40;
+    const speed = 2;
+    const oneStepTime = 4;
+    const oneStep = oneStepTime * speed;
+
+    let aTort = {
+        way : 0,
+        sleep : 0,
+        extra : 0,
+        time : 0
+    };
+
+    let bTort = {
+        way : 0,
+        sleep : 0,
+        extra : 0,
+        time : 0
+    };
+    
+    // for (let i = 0; i < 5; i++){
+    setTimeout(run, (aTort.time)*1010);
+    setTimeout(run, (bTort.time)*1010)
+    
+    function run(){
+    if (aTort.way < maxWay){
+            aTort.way += oneStep;
+            aTort.way += aTort.extra;
+            aTort.time = (aTort.extra/speed) + oneStepTime;
+            console.log(`Первая черепаха отпозла на ${aTort.way}`)
+        }else{
+            return aTort;
+    }
+
+    if (bTort.way < maxWay){
+        bTort.way += oneStep;
+        bTort.way += bTort.extra;
+        bTort.time = (bTort.extra/speed) + oneStepTime;
+        console.log(`Вторая черепаха отпозла на ${bTort.way}`)
+    }else{
+        return bTort;
+    }
+    
+    sleep();
+    }
+
+    function sleep(){
+        aTort.sleep = getRandomInt(3, 5);
+        bTort.sleep = getRandomInt(3, 5);
+
+        if (aTort.sleep > 4){
+        aTort.extra = ((Math.floor((aTort.sleep*60 - 60/4)/15))*250)/1000;
+        }
+
+        if (bTort.sleep > 4){
+            bTort.extra = ((Math.floor((bTort.sleep*60 - 60/4)/15))*250)/1000;
+            bTort.time = bTort.extra/speed;
+        }
+    }
+
+    }
+
+console.log(tortWay())
+
+
+
+
+
+
+
 
 
 
